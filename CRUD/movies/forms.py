85c73d7f064c2cf062a74f09bacf365945cc317d
Snail <soykim76@gmail.com>
2020-06-18 14:19:21 +0900

    실습입니다

---
+++ CRUD/movies/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from .models import Movie
+
+class MovieForm(forms.ModelForm):
+    title = forms.CharField(
+        label='영화명',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'my-movie',
+            }
+        )        
+    )
+    title_en = forms.CharField(
+        label='영화명(영문)',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'my-movie',
+            }
+        )
+    )
+    audience = forms.IntegerField(
+        label='누적 관객수'
+    )
+    open_date = forms.DateField(
+        label='개봉일'
+    )
+    genre = forms.CharField(
+        label='장르'
+    )
+    watch_grade = forms.CharField(
+        label='관람등급'
+    )
+    score = forms.FloatField(
+        label='평점'
+    )
+    poster_url = forms.CharField(
+        label='포스터 이미지 URL'
+    )
+    description = forms.CharField(
+        label='영화 소개',
+        widget=forms.Textarea(
+            attrs={
+                'class': ''                
+            }
+        )
+    )
+
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
---
+++ CRUD/movies/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=1000)
+    title_en = models.CharField(max_length=1000)
+    audience = models.IntegerField()
+    open_date = models.DateField()
+    genre = models.CharField(max_length=100)
+    watch_grade = models.CharField(max_length=100)
+    score = models.FloatField()
+    poster_url = models.TextField()
+    description = models.TextField()
+    
+
